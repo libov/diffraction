8a9ed68d3b61fd952c254b417a44ab16efed94d4
Vladyslav Libov <vladyslav@linux-mebx>
2014-05-06 20:51:19 +0200

    changed name of the directory hosting TXMLParser

--- code/env.sh
+++ code/env.sh
@@ -2,4 +2,4 @@
 # changes are ignored by git and won't be commited
 
 # TXMLParser installation (headers + library)
-export TXMLPARSER=/home/vladyslav/git_repositories/txmlparser
+export TXMLPARSER=/home/vladyslav/git_repositories/TXMLParser
