d6e496c696def3165e9ea72e224228d8fcb47356
Vladyslav Libov <vladyslav@linux-mebx>
2014-05-06 20:49:53 +0200

    added env.sh

---
+++ code/env.sh
@@ -0,0 +1,5 @@
+# edit this file according to your installation
+# changes are ignored by git and won't be commited
+
+# TXMLParser installation (headers + library)
+export TXMLPARSER=/home/vladyslav/git_repositories/txmlparser
